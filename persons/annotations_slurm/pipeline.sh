2ce10312fb2da4e9e76247dbe30a659a21c52b4c
Small-Samori <issahsamori@gmail.com>
2025-12-18 21:50:35 -0500

    updates

--- persons/annotations_slurm/pipeline.sh
+++ persons/annotations_slurm/pipeline.sh
@@ -5,12 +5,10 @@ COMMENT_FOLDER=$TIKTOK_FOLDER/comments
 PERSONS_FOLDER=$TIKTOK_FOLDER/persons
 MINICONDA_PATH=$GROUP_HOME/samori/miniconda3
 
-# COMMENTS_FILE=$COMMENT_FOLDER/cleaned/comments_5_cleaned.csv
 COMMENTS_FILE=$COMMENT_FOLDER/combined_comments_7.csv
 
 ACTIVATE_ENV="source $MINICONDA_PATH/bin/activate tiktok_env2"
 RUN_ANNOTATION="python ./annotation.py $COMMENTS_FILE $PERSONS_FOLDER"
-# RUN_ANNOTATION="python ./annotation2.py $COMMENTS_FILE $PERSONS_FOLDER"
 
 JOB_ID1=$(sbatch --parsable --partition=rbaltman --time 168:00:00 --mem 64G \
         --job-name gpt_annotation --wrap "$ACTIVATE_ENV;$RUN_ANNOTATION" \
